ad799a05f69078f55a82c10595cd5ef2f1070574
Rohit <rohitparih@gmail.com>
2024-05-01 23:03:45 +0530

    Implementation -> `Router`

---
+++ src/utils/Router.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import LandingPage from "../container/LandingContainer/LandingPage.jsx";
+
+const Router = () => {
+    return (
+        <Routes>
+            <Route path={"/"} element={<LandingPage />} />
+        </Routes>
+    );
+};
+
+export default Router;
