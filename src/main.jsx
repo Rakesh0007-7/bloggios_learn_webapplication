abcaf03d84c512750688a2e390e2dfddc947435e
Rohit <rohitparih@gmail.com>
2024-05-01 23:03:59 +0530

    Implementation -> `Router`

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {BrowserRouter} from "react-router-dom";
 
 export const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
       <BrowserRouter>
